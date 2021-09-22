62c7c5d85ed166770a745a8107b969c10385ab53
Farnoud Taghdissi Borhani <Farnood_t@yahoo.com>
2021-09-21 20:45:19 -0700

    Add project files.

---
+++ Amazon-Minimum Swap/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Amazon_Minimum_Swap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[] { 7, 1, 3, 2, 4, 5, 6 };
+            Console.WriteLine(MinimumSwap(arr));
+        }
+
+        static int MinimumSwap(int[] arr)
+        {
+            int min = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if(arr[i] != i + 1)
+                {
+                    for (int j = i+1; j < arr.Length; j++)
+                    {
+                        if(arr[j] != j+1)
+                        {
+                            int temp = arr[i];
+                            arr[i] = arr[j];
+                            arr[j] = temp;
+                            min++;
+                            break;
+                        }
+                    }
+                }
+            }
+
+            return min;
+        }
+
+    }
+}
